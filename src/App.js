4963154277d6bf41f18f26fa007487bec89a63d5
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-11-15 19:24:36 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
-import LineChart from "./LineChart";
-import Chart from "./ResponsiveBar";
-import Dendrogram from "./Dendrogram";
-import DendrogramTwo from "./DendrogramTwo";
-import DendrogramThree from "./DendrogramThree";
-import DendrogramFour from "./DendrogramFour";
+import LineChart from "./LineChart/LineChart";
+import ResponsiveBar from "./Bar/ResponsiveBar";
+import StackedBar from "./Bar/StackedBar";
+import StackedBarChart from "./Bar/StackedBarChart";
+import Dendrogram from "./Dendrogram/Dendrogram";
+import DendrogramTwo from "./Dendrogram/DendrogramTwo";
+import DendrogramThree from "./Dendrogram/DendrogramThree";
+import DendrogramFour from "./Dendrogram/DendrogramFour";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <StackedBarChart />
+        <StackedBar />
         <DendrogramFour />
         <DendrogramThree />
         <DendrogramTwo />
         <Dendrogram />
-        <Chart />
+        <ResponsiveBar />
         <LineChart />
       </header>
     </div>
---
+++ src/Bar/StackedBar.js
@@ -0,0 +1,144 @@
+import React, { useState, useEffect, useRef } from "react";
+import * as d3 from "d3";
+
+const data = [
+  {
+    year: 1980,
+    "🥑": 10,
+    "🍌": 20,
+    "🍆": 30,
+  },
+  {
+    year: 1990,
+    "🥑": 20,
+    "🍌": 40,
+    "🍆": 60,
+  },
+  {
+    year: 2000,
+    "🥑": 30,
+    "🍌": 45,
+    "🍆": 80,
+  },
+  {
+    year: 2010,
+    "🥑": 40,
+    "🍌": 60,
+    "🍆": 100,
+  },
+  {
+    year: 2020,
+    "🥑": 50,
+    "🍌": 80,
+    "🍆": 120,
+  },
+];
+
+const allKeys = ["🥑", "🍌", "🍆"];
+
+const colors = {
+  "🥑": "green",
+  "🍌": "orange",
+  "🍆": "purple",
+};
+
+export default function StackedBar() {
+  const [dimensions, setDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+  const svgRef = useRef();
+  const wrapperRef = useRef();
+  const update = useRef(false);
+
+  useEffect(() => {
+    // Listen for any resize event update
+    window.addEventListener("resize", () => {
+      setDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+      console.log(dimensions.width, dimensions.height);
+
+      // If resize, remove the previous chart
+      if (update.current) {
+        d3.selectAll("g").remove();
+      } else {
+        update.current = true;
+      }
+    });
+
+    // // Draw chart using the data and updated dimensions
+    // DrawChart(sample, dimensions);
+  }, [dimensions]);
+
+  //create svg layout
+  const margin = { top: 1, right: 80, bottom: 27, left: 20 };
+  let barWidth = window.innerWidth * 0.91 - margin.left - margin.right;
+  let barHeight = 320 - margin.top - margin.bottom;
+
+  const svg = d3
+    .select(svgRef.current)
+    .attr("width", barWidth + margin.left + margin.right)
+    .attr("height", barHeight + margin.top + margin.bottom)
+    .style("background-color", "pink")
+    .attr("transform", "translate(50,50)");
+
+  // stacks/layers
+  const stackGenerator = d3.stack().keys(allKeys);
+  const layers = stackGenerator(data);
+  const extent = [
+    0,
+    d3.max(layers, (layer) => d3.max(layer, (sequence) => sequence[1])),
+  ];
+
+  //scales
+  const xScale = d3
+    .scaleBand()
+    .domain(data.map((d) => d.year))
+    .range([margin.left, barWidth - margin.right])
+    .padding(0.1);
+
+  const yScale = d3.scaleLinear().domain(extent).range([barHeight, 0]);
+
+  //axes
+  const xAxis = d3.axisBottom(xScale);
+  svg
+    .select(".x-axis")
+    .attr("transform", `translate(20, ${barHeight})`)
+    .call(xAxis)
+    .attr("fill", "none")
+    .style("color", "black");
+
+  const yAxis = d3.axisLeft(yScale);
+  svg
+    .select(".y-axis")
+    .attr("transform", "translate(40, 0)")
+    .call(yAxis)
+    .attr("fill", "none")
+    .style("color", "black");
+
+  //rendering
+  svg
+    .selectAll(".layer")
+    .data(layers)
+    .join("g")
+    .attr("class", "layer")
+    .attr("fill", (layer) => colors[layer.key])
+    .selectAll("rect")
+    .data((layer) => layer)
+    .join("rect")
+    .attr("x", (sequence) => xScale(sequence.data.year))
+    .attr("width", xScale.bandwidth())
+    .attr("y", (sequence) => yScale(sequence[1]))
+    .attr("height", (sequence) => yScale(sequence[0]) - yScale(sequence[1]));
+
+  return (
+    <div ref={wrapperRef} style={{ marginBottom: "2rem" }}>
+      <svg ref={svgRef}>
+        <g className="x-axis" />
+        <g className="y-axis" />
+      </svg>
+    </div>
+  );
+}
