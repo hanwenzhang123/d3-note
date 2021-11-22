210387390d8ea290826a7ffb968cd02126968f64
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-11-22 18:18:38 -0500

    Create 04-create-bar-chart.js

---
+++ notes/04-create-bar-chart.js
@@ -0,0 +1,93 @@
+//index.js
+// select the svg container first
+const svg = d3.select('.canvas')
+  .append('svg')
+    .attr('width', 600)
+    .attr('height', 600);
+
+// create margins & dimensions
+const margin = {top: 20, right: 20, bottom: 100, left: 100};
+const graphWidth = 600 - margin.left - margin.right;
+const graphHeight = 600 - margin.top - margin.bottom;
+
+const graph = svg.append('g')
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);   //translate from left then top, x then y
+
+// create axes groups
+const xAxisGroup = graph.append('g')
+  .attr('transform', `translate(0, ${graphHeight})`)  //translate group down to the bottom, no change to the x, only y
+
+const yAxisGroup = graph.append('g');
+
+
+d3.json('menu.json').then(data => {
+
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.orders)])
+    .range([graphHeight, 0]);   //direction of the output
+
+  const x = d3.scaleBand()
+    .domain(data.map(item => item.name))
+    .range([0, 500])
+    .paddingInner(0.2)
+    .paddingOuter(0.2);
+
+  
+  // join the data to circs
+  const rects = graph.selectAll('rect')
+    .data(data);
+
+  // add attrs to circs already in the DOM
+  rects.attr('width', x.bandwidth)
+    .attr("height", d => graphHeight - y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x', d => x(d.name))
+    .attr('y', d => y(d.orders));   //where the y should start
+
+  // append the enter selection to the DOM
+  rects.enter()
+    .append('rect')
+      .attr('width', x.bandwidth)
+      .attr("height", d => graphHeight - y(d.orders))
+      .attr('fill', 'orange')
+      .attr('x', (d) => x(d.name))
+      .attr('y', d => y(d.orders));   //where the y should start
+  
+  
+  // create & call axes
+  const xAxis = d3.axisBottom(x);
+  const yAxis = d3.axisLeft(y)
+    .ticks(3)     //try to show only 3 ticks
+    .tickFormat(d => d + ' orders');
+
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
+  
+  xAxisGroup.selectAll('text')
+    .attr('fill', 'orange')
+    .attr('transform', 'rotate(-40)')
+    .attr('text-anchor', 'end')
+
+});
+
+
+//index.html
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>SVG Basics</title>
+</head>
+<body>
+  
+    <div class="canvas">
+
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/5.7.0/d3.js"></script>
+    <script src="index.js"></script>
+</body>
+</html>
