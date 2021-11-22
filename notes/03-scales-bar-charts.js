e31a6df7b346686512dd01c51d20b186c54ce7f9
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-11-22 16:46:31 -0500

    Update 03-scales-bar-charts.js

--- notes/03-scales-bar-charts.js
+++ notes/03-scales-bar-charts.js
@@ -23,31 +23,33 @@ d3.json('menu.json').then(data => {
    .paddingInner(0.2)
    .paddingOuter(0.2);
   
-//   console.log(x('veg burger'));
-//   console.log(x('veg curry'));
+//   console.log(x('veg burger'));  //125
+//   console.log(x('veg curry'));   //250
+//   console.log(x.bandwidth());    //125 - each bar is 125px
+  
+  const min = d3.min(data, d => d.orders)   //console.log(min)  //200
+  const max = d3.max(data, d => d.orders)   //console.log(max)  //900
+  const extent = d3.extent(data, d => d.orders)  //[200, 900] find the lowest on the highest 
 
   // join the data to circs
   const rects = svg.selectAll('rect')
     .data(data);
 
   // add attrs to circs already in the DOM
-  rects.attr('width', 50)
+  rects.attr('width', x.bandwidth)  // no need for hard px to set the width for the bar since we use bandwidth
     .attr("height", d => y(d.orders))   //scale the y linear scale
     .attr('fill', 'orange')
-    .attr('x', (d, i) => i * 70)
+    .attr('x', d => x(d.name))   //where the x should stop, have the bars next to each other // no need since bandwidth .attr('x', (d, i) => i * 70)
 
   // append the enter selection to the DOM
   rects.enter()
     .append('rect')
-      .attr('width', 50)
+      .attr('width', x.bandwidth)    // no need for hard px to set the width for the bar since we use bandwidth
       .attr("height", d => y(d.orders)) //scale the y linear scale
       .attr('fill', 'orange')
-      .attr('x', (d, i) => i * 70)
-
+      .attr('x', d => x(d.name))   //where the x should stop, have the bars next to each other // no need since bandwidth .attr('x', (d, i) => i * 70)
+  
 });
-
-
-
 
 
 //index.html
