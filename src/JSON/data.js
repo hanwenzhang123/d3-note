874742c6fd974b06de48d33ed8c28ef67c473750
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-11-24 13:38:27 -0500

    Create data.js

---
+++ src/JSON/data.js
@@ -0,0 +1,17 @@
+data = [
+        {
+          key: "Apple Books",
+          values: [
+             {group: 'Apple Books', value: 'ABooks For You USA'}
+             {group: 'Apple Books', value: 'ABooks For You USA'}
+             {group: 'Apple Books', value: 'ABooks For You USA'}
+          ],
+        },
+        {
+          key: "Apple TV App",
+          values: [
+              {group: 'Apple TV App', value: 'ATV WTWP BRA'}
+              {group: 'Apple TV App', value: 'ATV WTWP BRA'}
+          ],
+        },
+      ];
